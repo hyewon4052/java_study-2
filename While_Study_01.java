df8bf32a13cebb090261d11777ab75e3888f46d3
hyewon4052 <s2311@e-mirim.hs.kr>
2023-08-31 15:34:59 +0900

    While_Study_01

---
+++ While_Study_01.java
@@ -0,0 +1,37 @@
+
+public class While_Study_01 {
+
+	public static void main(String[] args) { 
+		
+//		int i = 1;
+//		while(i <= 10 ) {
+//			System.out.println(i);
+//			i++;
+//		}
+
+		// while문을 이용하여 1~10 사이의 홀수만 출력하시오
+		
+//		int i = 1;
+//		while (i <= 10) {
+//			if (i % 2 == 1)
+//				System.out.println(i);
+//			i++;
+//		}
+		
+//		int i = 0;
+//		while (true) {
+//			i++;
+//			if (i == 10)
+//				break;
+//			else if (i % 2 == 0) continue;
+//			System.out.println(i);
+//		}
+		
+		int w = 10;
+		do {
+			System.out.println("do while의 w " + w);
+		}
+		while (w <= 5);	
+	}
+
+}
