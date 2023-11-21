ab160389761834c16a4f0256944afc306873d340
hyewon4052 <s2311@e-mirim.hs.kr>
2023-11-21 20:00:00 +0900

    init commit

---
+++ Car.java
@@ -0,0 +1,23 @@
+
+public class Car {
+	String color;
+	String gearType;
+	int door;
+	
+	public Car() {
+		this("White","auto",4);
+	}
+	
+	
+	public Car(String color) {
+		this(color,"auto",4);
+	}
+
+
+	public Car(String color, String gearType, int door) {
+		this.color = color;
+		this.gearType = gearType;
+		this.door = door;
+	}
+	
+}
