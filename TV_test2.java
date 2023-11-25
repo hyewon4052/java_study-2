4a8b3002a9b6a98a2b9fc0dee956c9c8246484c3
hyewon4052 <s2311@e-mirim.hs.kr>
2023-11-25 20:00:00 +0900

    init commit

---
+++ TV_test2.java
@@ -0,0 +1,15 @@
+
+public class TV_test2 {
+	public static void main(String[] args) {
+		TV tv1 = new TV();
+		TV tv2 = new TV();
+		
+		System.out.println("tv1의 channel 값은 " + tv1.channel + "입니다.");
+		System.out.println("tv1의 channel 값은 " + tv2.channel + "입니다.");
+		
+		tv1.channel = 7;
+		
+		System.out.println("tv1의 channel 값은 " + tv1.channel + "입니다.");
+		System.out.println("tv1의 channel 값은 " + tv2.channel + "입니다.");
+	}
+}
