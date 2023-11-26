189ff2776dddd9459534c7622713e62f280bac8b
hyewon4052 <s2311@e-mirim.hs.kr>
2023-11-26 20:00:00 +0900

    init commit

---
+++ ClassExample2.java
@@ -0,0 +1,11 @@
+
+public class ClassExample2 {
+
+	public static void main(String[] args) {
+			
+		GoodStock obj = new GoodStock("A52135", 200);
+		System.out.println("상품코드 : " + obj.goodsCode + ", 재고수량 : " + obj.stockNum);
+		
+	}
+
+}
